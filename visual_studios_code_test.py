cce5878b63e18cc7cf51991b69df0e77b2d89128
Foxsimile <misterjfrenard@gmail.com>
2019-10-29 20:55:54 -0400

    Added a useless doc-string in order to understand the methodologies of git/git-hub.

--- visual_studios_code_test.py
+++ visual_studios_code_test.py
@@ -1,5 +1,10 @@
 import requests
 
+"""
+This comment represents an attempt to better understand the staging/commit
+process of git/git-hub. As such, it would be most expedient to ignore it entirely.
+"""
+
 r = requests.get("https://coreyms.com")
 print(r.status_code)
 print(r.ok)
